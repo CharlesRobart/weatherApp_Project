23662424c5c8d4827a18db3a842d89f362d1aa38
unknown <antoine_grubert@hotmail.fr>
2023-12-08 14:22:42 +0100

    feat : ajout de la position par défaut sur l'appli

---
+++ src/components/MyPositionCard/MyPositionCard.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import { useState, useEffect  } from 'react'
+import './MyPositionCard.scss'
+import { Link } from 'react-router-dom'
+
+const API_KEY = import.meta.env.VITE_API_KEY
+
+const MyPositionCard = ({cityData}) => {
+    //States de l'appli
+    const [currentWeather, setCurrentWeather] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    //Appel API
+    const fetchCurrentWeather = async () => {
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${cityData.lat}&lon=${cityData.lon}&appid=${API_KEY}&lang=fr&units=metric`);
+        setCurrentWeather(response.data);
+        }
+
+        catch(err) {
+            console.error(err)
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+
+    useEffect(() => {
+        fetchCurrentWeather(currentWeather)
+    }, [cityData]);
+
+    return (
+
+        <>  
+            {!loading && 
+            <section id = {currentWeather.id} className='city-card'>
+                <Link className='city-card-link' to={`/${cityData.name}/${cityData.lat}/${cityData.lon}`}> 
+                    <div className='city-card-flex'>
+                        <h2 className='city-card-title'> {cityData.name} </h2>
+                        <p className='city-card-description'>{currentWeather.weather[0].description} </p>
+                    </div>
+                    <div className='city-card-flex'>
+                        <p className='city-card-temp'> {Math.floor(currentWeather.main.temp)} °</p>
+                        <img className='city-card-img'src={`https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`} alt="icon" />
+                    </div>
+                </Link>
+            </section>}            
+        </>
+    )
+
+}
+
+export default MyPositionCard
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,12 @@ import './HomePage.scss'
 import { useState , useEffect } from 'react'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import CityCard from '../../components/CityCard/CityCard'
+import MyPositionCard from '../../components/MyPositionCard/MyPositionCard'
 
 const HomePage = () => {
+
+    //State ma position
+    const [myPosition, setMyPosition] = useState(null)
 
     //State des villes
     const [cities, setCities] = useState(() => {
@@ -14,10 +18,15 @@ const HomePage = () => {
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position) => {
-            console.log('lat: ' ,position.coords.latitude);
-            console.log('lon: ' ,position.coords.longitude);
-        });
+            setMyPosition({
+                name: 'Ma position',
+                lat: position.coords.latitude,
+                lon: position.coords.longitude
+            });
+        }); 
+    },[]);
 
+    useEffect(() => {
         localStorage.setItem('cities' , JSON.stringify(cities));
     },[cities])
 
@@ -31,10 +40,9 @@ const HomePage = () => {
             <h1 className='home-page-title'> Météo </h1>
             <SearchBar cities={cities} setCities = {setCities} />
             <div className='city-card-container'>
+                {myPosition && <MyPositionCard cityData={myPosition} />}
                 {cities.map((city, index) => {
-
                     return (
-                        
                         <CityCard key={index} cityData={city}  onDeleteCard={() => onDeleteCard(index)}/>
                     )
 
